bba8034e4bc309673f45d711c707c334d53cea51
GoHoo <potoo10@yahoo.com>
2022-07-03 13:00:42 +0300

    Create 3.py

---
+++ Day2/3.py
@@ -0,0 +1,9 @@
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+int_age = int(age)
+mori = 90 - int_age
+days = int_age * 365
+weeks = int_age * 52
+months = int_age * 12
+print(f"You have {age} days,{weeks} weeks, and {months} months left.")
