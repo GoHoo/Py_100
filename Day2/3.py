43c30a5b13f7acacea310b4ec73a1a12a42cdf2b
GoHoo <potoo10@yahoo.com>
2022-07-03 13:34:32 +0300

    Update 3.py

--- Day2/3.py
+++ Day2/3.py
@@ -1,3 +1,5 @@
+'''
+# 🚨 Don't change the code below 👇
 age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 #Write your code below this line 👇
@@ -6,4 +8,19 @@ mori = 90 - int_age
 days = int_age * 365
 weeks = int_age * 52
 months = int_age * 12
-print(f"You have {age} days,{weeks} weeks, and {months} months left.")
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+'''
+
+
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+int_age = int(age)
+mori = 90 - int_age
+days = int_age * 365
+weeks = int_age * 52
+months = int_age * 12
+print(f"You have {days} days,{weeks} weeks, and {months} months left.")
