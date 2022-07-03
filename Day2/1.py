e2e08c9931210beeba0effc4356e00c5e5a26e7d
GoHoo <potoo10@yahoo.com>
2022-07-03 10:48:24 +0300

    Create 1.py

---
+++ Day2/1.py
@@ -0,0 +1,12 @@
+#neded help but the thought process was in the right direction,90% done by myself 
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+a= int(two_digit_number[0])
+b=int(two_digit_number[1])
+res= a+b
+print(res)
